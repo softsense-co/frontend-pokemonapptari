d5073fa129bc0079ec74faa2ad81ca3605102539
sintariaulia <sintariaulia04@gmail.com>
2023-03-02 11:12:09 +0700

    Update App.js
    Menghapus code bawaan project dan Menambahkan code baru untuk menampilkan header dan content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Hero from './components/hero';
+import Experience from './components/Experience.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-white overflow-hidden ">
+      <Header />
+      <Hero />
+      <Experience />
+      
     </div>
   );
 }
