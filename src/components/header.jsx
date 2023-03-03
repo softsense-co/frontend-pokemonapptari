a493b187508a008bce5652e4fb942c627cee6f1f
sintariaulia <sintariaulia04@gmail.com>
2023-03-03 14:32:55 +0700

    Create file header
    Membuat file tampilan navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CenterMenu from "./centerMenu";
+import { Link } from "react-router-dom";
 
 function Header() {
     const buttonStyle = 'border-[2px] rounded-[10px] border-[#bad8d8] px-[20px] py-[7px]'
@@ -7,15 +7,28 @@ function Header() {
     return (
         <div className="header bg-[#ceeeed] flex items-center justify-between px-20 pt-5">
             {/* logo */}
-            <img src={require("../img/icon.png")} alt="" className="logo w-[100px] h-[80px] "/>
+            <Link to="/">
+                <img src={require("../img/icon.png")} alt="" className="logo w-[100px] h-[80px] " />
+            </Link>
 
-            {/* side menu */}
-             <CenterMenu />
+            {/* side center menu */}
+            <div className="menu flex">
+                <ul className="flex w-[100%] justify-between text-slate-800 font-semibold">
+                    <li className=" mr-12">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className=" mr-12">
+                        <Link to="/Pokemon">Pokemons</Link>
+                    </li>
+                    <li className=" mr-12">
+                        <Link to="/MyPokemons">My Pokemons</Link>
+                    </li>
+                </ul>
+            </div>
 
             {/* button */}
             <div className="buttons flex text-slate-700 ">
-                <button className={buttonStyle+` mr-2 hover:bg-[#9dc8c6]`}>Sign In</button>
-                <button className={buttonStyle+` mr-[35px] bg-[#9dc8c6] hover:bg-[#d6f5f3]`}>Sign Up</button>
+                <button className={buttonStyle + ` mr-2 hover:bg-[#9dc8c6]`}>Sign In</button>
             </div>
         </div>
     )
