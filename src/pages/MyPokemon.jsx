26f5baa4935b3a31f96d25c460546e0cd32f9e24
sintariaulia <sintariaulia04@gmail.com>
2023-03-03 14:35:45 +0700

    Create file page my pokemon
    Membuat file page untuk fitur my pokemons

---
+++ src/pages/MyPokemon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header'
+
+const MyPokemon = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default MyPokemon
