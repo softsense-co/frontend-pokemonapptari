0a47b26795ab75dd660b2edd7314aef02e9c5a6f
sintariaulia <sintariaulia04@gmail.com>
2023-03-03 14:34:21 +0700

    Create file pages hero
    Akan memanggil header dan componen home

---
+++ src/pages/hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/header";
+import Home from "../components/home";
+
+function Hero() {
+    return (
+        <div>
+            <Header />
+            <Home />
+        </div>
+
+
+
+    )
+}
+
+
+
+export default Hero
