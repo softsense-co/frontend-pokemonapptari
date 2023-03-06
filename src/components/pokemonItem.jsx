6db98428d1166bbe8a4a0681081fe0b4dcc8a896
sintariaulia <sintariaulia04@gmail.com>
2023-03-06 17:56:51 +0700

    Update Pokemon Item
    Update file pokemon item menambahkan detai pada salah satu pokedex

--- src/components/pokemonItem.jsx
+++ src/components/pokemonItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-export const PokemonItem = () => {
+const PokemonItem = () => {
 
     const traceButton = () => {
         console.log("Tombol Di Klik");
@@ -95,6 +95,28 @@ export const PokemonItem = () => {
         <div className="flex flex-col justify-start px-[5rem] py-8 relative">
             <h1 className='text-4xl font-bold text-slate-700 py-3'>Pokemons</h1>
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 md:px-10 py-8 gap-4 sm:gap-10 md:gap-12 lg:gap-10">
+
+                <div className='bg-white/75 p-5 rounded-[35px]'>
+                    <Link to="/DetailPokemon">
+                        <img src={require("../img/Charmeleon.png")} alt="" className='w-full md:w-[18rem] mx-auto py-7' />
+                        <h5 className='text-slate-700 text-lg md:text-xl text-center font-bold mb-2 uppercase'>Charmeleon</h5>
+                    </Link>
+                    <div className='text-sm md:text-base pb-2  pl-3'>
+                        <p className='text-slate-600'>
+                            Price
+                        </p>
+                        <p className='text-slate-600 font-bold'>
+                            US $33
+                        </p>
+                    </div>
+                    <div className='text-right pr-3'>
+                        <button
+                            type="button"
+                            className=' px-5 py-2.5 bg-[#55a8a3] text-slate-900 font-semibold text-sm uppercase rounded-xl hover:bg-[#bfdfde]'>
+                            Add
+                        </button>
+                    </div>
+                </div>
 
                 {/* Looping array object */}
                 {pokemons.map(pokemon => {
