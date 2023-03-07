7d23df01176555890111dc501bfb9993afbd638f
sintariaulia <sintariaulia04@gmail.com>
2023-03-07 17:45:34 +0700

    Add Sign Up page
    Menambahkan tampilan untuk Sign Up atau register

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Pokemon from './pages/Pokemon';
 import MyPokemon from './pages/MyPokemon';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DetailPokemon from './pages/detailPokemon';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path='/Pokemon' element={<Pokemon />} />
         <Route path='/MyPokemons' element={<MyPokemon />} />
         <Route path='/DetailPokemon' element={<DetailPokemon />} />
+        <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/SignUp' element={<SignUp />} />
       </Routes>
     </Router >
   );
---
+++ src/pages/signUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterItem from '../components/registerItem'
+
+const SignUp = () => {
+  return (
+    <div>
+        <RegisterItem />
+    </div>
+  )
+}
+
+export default SignUp
