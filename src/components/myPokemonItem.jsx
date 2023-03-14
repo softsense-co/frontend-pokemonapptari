4d61a09bd28cca98fcd051e44b9af434cf947db5
sintariaulia <sintariaulia04@gmail.com>
2023-03-14 17:53:58 +0700

    Update My Pokemon Page
    Add button delete

--- src/components/myPokemonItem.jsx
+++ src/components/myPokemonItem.jsx
@@ -3,7 +3,7 @@ import { FaMinus } from "react-icons/fa";
 
 const MyPokemonItem = () => {
 
-    const [pokemonData, setPokemonData] = useState([]);
+    const [pokemonDatas, setPokemonData] = useState([]);
 
     const handleGetPokemonData = () => {
         const dataFromStorage = localStorage.getItem("pokemonData");
@@ -19,11 +19,15 @@ const MyPokemonItem = () => {
         }
     }, [])
 
-    function deleteMyPokemon(id) {
-        const newData = pokemonData.filter(p => p.id !== id);
-        handleGetPokemonData([...newData]);
-        console.log("Delete Data");
-    };
+    const deleteMyPokemon = (id) => {
+        console.log(id);
+        const newArray = pokemonDatas.filter(p => p.id !== id);
+        setPokemonData([...newArray]);
+        // const deleteItems = pokemonDatas.filter((elem, ind) => {
+        //     return ind !== id;
+        // });
+        // setPokemonData(deleteItems);
+    }
 
     return (
         <div className="flex flex-col justify-start px-[5rem] py-8 relative">
@@ -31,10 +35,10 @@ const MyPokemonItem = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 py-8 gap-4 sm:gap-10 lg:gap-10">
 
                 {/* From Storage */}
-                {pokemonData.map(pokemon => {
+                {pokemonDatas.map(pokemon => {
                     return (
-                        <div className='bg-white/75 p-4 rounded-3xl'>
-                            <img key={pokemon.id} src={pokemon.image} alt="" className='h-52 mx-auto' />
+                        <div className='bg-white/75 p-4 rounded-3xl' key={pokemon.id}>
+                            <img src={pokemon.image} alt="" className='h-52 mx-auto' />
                             <h5 className='text-slate-700 text-base md:text-lg font-semibold py-3  text-center uppercase'>{pokemon.name}</h5>
                             <div className='text-sm md:text-base pb-2 text-left pl-3'>
                                 <p className='text-slate-600'>
@@ -47,7 +51,7 @@ const MyPokemonItem = () => {
                             <div className='text-right pr-3'>
 
                                 <button
-                                    onClick={() => deleteMyPokemon(pokemonData.id)}
+                                    onClick={() => deleteMyPokemon(pokemon.id)}
                                     type="button"
                                     className='px-3 py-2 bg-[#55a8a3] text-slate-700 font-medium text-sm uppercase rounded-md hover:bg-[#bfdfde]'>
                                     <FaMinus />
