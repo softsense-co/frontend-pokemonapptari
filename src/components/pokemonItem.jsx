39eed5e86c7e53222147eb6ba8dc5ab88eeebbfc
sintariaulia <sintariaulia04@gmail.com>
2023-03-14 16:43:04 +0700

    Update pokemonitem
    Add data to local storage

--- src/components/pokemonItem.jsx
+++ src/components/pokemonItem.jsx
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from "react-router-dom";
-
+import React from 'react'
 
 const PokemonItem = () => {
 
-    const [myPokemons, setMyPokemon] = useState((""));
-    const [pokemons, setPokemons] = useState([
+    const pokemonData = [
         {
             id: "1",
             name: "Charmeleon",
@@ -96,43 +93,28 @@ const PokemonItem = () => {
             image: '/img/Shinx.png',
             price: " US $38"
         },
-    ]);
+    ]
 
-    const handleChangePokemon = (values) => {
-        setPokemons(values)
-        localStorage.setItem("pokemonitems", JSON.stringify(values))
-    }
+    const addPokemons = (item) => {
+        const myPokemonslocalstorage = localStorage.getItem('pokemonData')
 
-    const handleGetPokemon = () => {
-        const dataFromStorage = localStorage.getItem("pokemonitems");
-        if (dataFromStorage) {
-            setMyPokemon(JSON.parse(dataFromStorage))
+        console.log(myPokemonslocalstorage)
+
+        if (myPokemonslocalstorage) {
+            const myPokemons = JSON.parse(myPokemonslocalstorage);
+            const newMyPokemons = [...myPokemons, { id: item.id, name: item.name, image: item.image, price: item.price }];
+            localStorage.setItem('pokemonData', JSON.stringify(newMyPokemons));
+        } else {
+            const newMyPokemons = [{ id: item.id, name: item.name, image: item.image, price: item.price }];
+            localStorage.setItem('pokemonData', JSON.stringify(newMyPokemons));
         }
+
+        // Cara Kedua :
+        // const myPokemons = JSON.parse(localStorage.getItem('pokemonData') || "[]");
+        // const newMyPokemons = [...myPokemons, { id: item.id, name: item.name, image: item.image, price: item.price }];
+        // localStorage.setItem('pokemonData', JSON.stringify(newMyPokemons));
+
     }
-
-    useEffect(() => {
-        handleGetPokemon()
-
-        return () => {
-
-        }
-    }, [])
-
-
-    function addPokemon() {
-        if (myPokemons !== "") {
-            handleChangePokemon([...pokemons, { myPokemons, id: `${myPokemons}-${Date.now()}` }]);
-            setMyPokemon("")
-        }
-        console.log("Tombol diklik");
-    };
-
-    function deletePokemon(id) {
-        const newPokemon = pokemons.filter(p => p.id !== id);
-        handleChangePokemon([...newPokemon]);
-    }
-
-
 
 
     return (
@@ -141,13 +123,11 @@ const PokemonItem = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 py-8 gap-4 sm:gap-10 lg:gap-10">
 
                 {/* Looping array object */}
-                {pokemons.map(pokemon => {
+                {pokemonData.map(pokemon => {
                     return (
                         <div className='bg-white/75 p-4 rounded-3xl'>
-                            <Link to="/DetailPokemon">
-                                <img src={pokemon.image} alt="" className='h-52 mx-auto' />
-                                <h5 className='text-slate-700 text-lg md:text-xl font-bold py-3  text-center uppercase'>{pokemon.name}</h5>
-                            </Link>
+                            <img src={pokemon.image} alt="" className='h-52 mx-auto' />
+                            <h5 className='text-slate-700 text-base md:text-lg font-semibold py-3  text-center uppercase'>{pokemon.name}</h5>
                             <div className='text-sm md:text-base pb-2 text-left pl-3'>
                                 <p className='text-slate-600'>
                                     Price
@@ -159,7 +139,7 @@ const PokemonItem = () => {
                             <div className='text-right pr-3'>
 
                                 <button
-                                    onClick={() => addPokemon()}
+                                    onClick={() => addPokemons(pokemon)}
                                     type="button"
                                     className='px-3 py-2 bg-[#55a8a3] text-slate-700 font-medium text-sm uppercase rounded-md hover:bg-[#bfdfde]'>
                                     Add
@@ -170,22 +150,6 @@ const PokemonItem = () => {
                     );
                 })}
 
-                {/* Looping array object */}
-                {pokemons.map(myPokemons => {
-                    return (
-                        <div className='bg-white/75 p-4 rounded-[35px]'>
-                            <li key={myPokemons.id}>{myPokemons.myPokemons}</li>
-                            <div className='text-right pr-3'>
-                                <button
-                                    onClick={() => deletePokemon(myPokemons.id)}
-                                    type="button"
-                                    className='px-3 py-2.5 bg-[#b2d7d5] text-slate-800 font-semibold text-sm uppercase rounded-xl hover:bg-[#bfdfde]'>
-                                    DELETE
-                                </button>
-                            </div>
-                        </div>
-                    );
-                })}
 
             </div>
         </div>
