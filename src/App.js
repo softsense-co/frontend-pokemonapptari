a171e43f2fc869656ff4324f61414dcac09217e3
sintariaulia <sintariaulia04@gmail.com>
2023-03-15 17:08:23 +0700

    Pokemon Detail

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Hero from './pages/hero';
 import Pokemon from './pages/Pokemon';
 import MyPokemon from './pages/MyPokemon';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DetailPokemon from './pages/DetailPokemon';
 import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
 import Todo from './pages/Todo';
@@ -16,7 +15,6 @@ function App() {
         <Route path='/Todo' element={<Todo />} />
         <Route path='/Pokemon' element={<Pokemon />} />
         <Route path='/MyPokemons' element={<MyPokemon />} />
-        <Route path='/DetailPokemon' element={<DetailPokemon />} />
         <Route path='/SignIn' element={<SignIn />} />
         <Route path='/SignUp' element={<SignUp />} />
       </Routes>
