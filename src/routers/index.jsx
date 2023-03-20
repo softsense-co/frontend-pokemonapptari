8ef4413cddd678dbb052bed3738958244d7c9d0d
sintariaulia <sintariaulia04@gmail.com>
2023-03-20 23:18:28 +0700

    fix:foldering structure

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from '../pages/hero';
 import Index from '../pages/dashboard/pokemons';
 import IdPokemon from '../pages/dashboard/pokemons/[id]';
-import MyPokemons from '../pages/dashboard/mypokemons';
 import Todos from '../pages/dashboard/todos'
 import SignIns from '../pages/auth/signins';
 import SignUps from '../pages/auth/signups';
@@ -17,7 +16,6 @@ function IndexRouter() {
             <Route path='/Todos' element={<Todos />} />
             <Route path='/Pokemons' element={<Index />} /> 
             <Route path='/IdDetailPokemon' element={<IdPokemon />} />
-            <Route path='/MyPokemons' element={<MyPokemons />} />
 
             <Route path='/SignIn' element={<SignIns />} />
             <Route path='/SignUp' element={<SignUps />} />
