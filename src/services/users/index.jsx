eed4a4e7847b4b74c8c602e76b3dfa2e57dc797b
sintariaulia <sintariaulia04@gmail.com>
2023-03-28 10:18:56 +0700

    fix: fix feature user

--- src/services/users/index.jsx
+++ src/services/users/index.jsx
@@ -32,9 +32,9 @@ const IndexUser = () => {
             <Header />
             <div>
                 <div className='bg-white/50 my-10 rounded-3xl shadow-2xl max-w-md mx-auto sm:max-w-lg md:max-w-2xl lg:max-w-4xl'>
-                    <h1 className='text-3xl font-extrabold text-center py-5 text-slate-600'>Users</h1>
+                    <h1 className='text-3xl font-extrabold text-center py-9 text-slate-600'>Users</h1>
                     <div >
-                        <div className='grid grid-cols-3 gap-7 py-10 px-8'>
+                        <div className='grid grid-cols-3 gap-7 pb-12 px-8'>
                             {arrUser}
                         </div>
 
